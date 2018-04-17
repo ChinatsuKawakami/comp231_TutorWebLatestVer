74cb7d7390339e95b50602420526b3cb39a569d4
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2018-04-16 20:19:47 -0400

    version 0.0.5 I added data grid view to show tutor info in subject page.

--- Repo-111-master/comp231_tutorweb-master/TutorWeb/TutorWebsite/Subject.aspx
+++ Repo-111-master/comp231_tutorweb-master/TutorWeb/TutorWebsite/Subject.aspx
@@ -2,5 +2,29 @@
 
 <asp:Content ID="subject" runat="server" ContentPlaceHolderID="ContentPlaceHolder1">
     <asp:Label runat="server">Subjects page</asp:Label>
-
+    <br/>
+    <asp:DropDownList ID="subjectDDL" runat="server" AutoPostBack="true"  DataSourceID="SqlDataSource1" DataTextField="Subject" DataValueField="SubjectId">
+    </asp:DropDownList>
+    <br />
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:SqlConnectionString %>" SelectCommand="SELECT [Subject], [SubjectId] FROM [Subject]"></asp:SqlDataSource>
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="SqlDataSource2" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3" HorizontalAlign="Center">
+        <Columns>
+            <asp:BoundField DataField="FirstName" HeaderText="FirstName" SortExpression="FirstName" />
+            <asp:BoundField DataField="LastName" HeaderText="LastName" SortExpression="LastName" />
+        </Columns>
+        <FooterStyle BackColor="White" ForeColor="#000066" />
+        <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+        <RowStyle ForeColor="#000066" />
+        <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+        <SortedAscendingCellStyle BackColor="#F1F1F1" />
+        <SortedAscendingHeaderStyle BackColor="#007DBB" />
+        <SortedDescendingCellStyle BackColor="#CAC9C9" />
+        <SortedDescendingHeaderStyle BackColor="#00547E" />
+    </asp:GridView>
+    <asp:SqlDataSource ID="SqlDataSource2" runat="server" ConnectionString="<%$ ConnectionStrings:SqlConnectionString %>" SelectCommand="SELECT [FirstName], [LastName], [subjectId] FROM [Tutor] WHERE ([subjectId] = @subjectId)">
+        <SelectParameters>
+            <asp:ControlParameter ControlID="subjectDDL" Name="subjectId" PropertyName="SelectedValue" Type="Int32" />
+        </SelectParameters>
+    </asp:SqlDataSource>
 </asp:Content>
--- Repo-111-master/comp231_tutorweb-master/TutorWeb/TutorWebsite/Subject.aspx.cs
+++ Repo-111-master/comp231_tutorweb-master/TutorWeb/TutorWebsite/Subject.aspx.cs
@@ -4,11 +4,80 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
 
 public partial class Subject : System.Web.UI.Page
 {
+
+    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlConnectionString"].ToString());
     protected void Page_Load(object sender, EventArgs e)
     {
 
+        //if(!Page.IsPostBack)
+        //{
+        //   // LoadGridView();
+        //   // LoadSubjects();
+        //}
+
+
+        //  LoadData();
+        // byte[] byt = System.Text.Encoding.UTF8.GetBytes(0x433A5C55736572735C6368696E617473755C536F757263655C5265706F735C5265706F2D3131312D6D61737465725C5265706F2D3131312D6D61737465725C636F6D703233315F7475746F727765622D6D61737465725C5475746F725765625C5475746F72576562736974655C496D616765735C7475746F72696D675C6D616E);
+        // //Context.Response.ContentType="image/jpeg";
+        // string encoded = HttpUtility.UrlEncode(Convert.ToBase64String(byt));
+        // Console.Write(encoded);
+        //// string str = Convert.ToBase64String(0x433A5C55736572735C6368696E617473755C536F757263655C5265706F735C5265706F2D3131312D6D61737465725C5265706F2D3131312D6D61737465725C636F6D703233315F7475746F727765622D6D61737465725C5475746F725765625C5475746F72576562736974655C496D616765735C7475746F72696D675C6D616E);
+
+
     }
+    //private void LoadSubjects()
+    //{
+    //    subjectDDL.DataSource = LoadData();
+    //    //subjectDDL.DataTextField = "firstname";
+    //    //subjectDDL.DataTextField = "lastname";
+    //    subjectDDL.DataValueField = "subjectid";
+    //    subjectDDL.DataTextField = "subject";
+    //    subjectDDL.DataBind();
+    //}
+
+    //private void LoadGridView()
+    //{
+    //    SubjectGridView.DataSource = LoadData();
+    //    SubjectGridView.DataBind();
+    //    //throw new NotImplementedException();
+    //}
+
+    //private void LoadGridView(int id)
+    //{
+    //    SubjectGridView.DataSource = LoadData();
+    //    SubjectGridView.DataBind();
+    //    //throw new NotImplementedException();
+    //}
+
+    //private DataSet LoadData()
+    //{
+    //    //SqlDataAdapter adap = new SqlDataAdapter("select Tutor.FirstName, Tutor.LastName from tutor Tutor.subjectid = subject.subjectid",con);
+    //    SqlDataAdapter adap = new SqlDataAdapter("select subject from subject", con);
+    //    DataSet ds = new DataSet();
+    //    adap.Fill(ds);
+    //    return ds;
+    //}
+    //private DataSet LoadData(int id)
+    //{
+    //    SqlDataAdapter adap = new SqlDataAdapter("select Tutor.FirstName, Tutor.LastName from tutor where subjectid = " +id , con);
+    //    DataSet ds = new DataSet();
+    //    adap.Fill(ds);
+    //    return ds;
+    //}
+
+    //protected void subjectDDL_SelectedIndexChanged(object sender, EventArgs e)
+    //{
+    //    LoadGridView(int.Parse(subjectDDL.SelectedValue));
+    //}
+
+
+
+
 }
